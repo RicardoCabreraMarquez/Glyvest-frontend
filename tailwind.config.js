457397d3ceb5b403ba0661aa25c01ada869b4da0
RicardoCabreraMarquez <95092615+RicardoCabreraMarquez@users.noreply.github.com>
2023-06-11 22:31:56 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export default {
         'fondo': 'rgba(0, 0, 0, 0.80)',
       },
       backgroundImage: {
-        'login': "url('./public/image/fondo.svg')",
+        'login': "url('../public/image/fondo.svg')",
       },
       width: {
         'login': '31.25rem',
